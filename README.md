acb836ec3514d402494eca0ca4721fe75eef8a0f
MikeNike <kooduser@pupil.kood.tech>
2023-11-21 13:50:54 +0200

    :)

---
+++ README.md
@@ -0,0 +1,33 @@
+# Groupie-tracker-visualizations
+
+## Author
+
+### Markus Tali (mtali) and Kaileen Umal (kumal)
+
+#### <a href="https://01.kood.tech/git/mtali">My Gitea Repo</a>
+
+## Info
+
+Groupie Trackers is a web-based project that uses a provided API to display information about bands and artists. The API covers details like artist backgrounds, concert locations, dates, and how they interrelate. The website aims to present this data in a user-friendly manner using various visualization methods. A significant feature is its client-server interactions, where user actions fetch data from the server, following a request-response model.
+Groupie tracker visualizations consists of manipulating the data coming from the API and displaying it in the most presentable way possible to you.
+
+## See [audit requirements]:
+
+<a href="https://github.com/01-edu/public/tree/master/subjects/groupie-tracker/visualizations/audit">Audit Repo</a>
+
+## How to view webpage
+
+1. **Clone the project:**
+   ` git clone https://01.kood.tech/git/mtali/`
+
+<a href=https://01.kood.tech/git/mtali/groupie-tracker-visualizations.git></a>
+
+2. **Open terminal and write command:**
+   `go run main.go`
+
+3. **Server has started, visit http://localhost:8080/**
+   <a href=http://localhost:8080/></a>
+
+4. **Select a band by clicking on its image or button to view more details**
+
+5. **To shut down the server, press Ctrl + C in the terminal**
---
+++ main.go
@@ -0,0 +1,146 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	"text/template"
+	"time"
+)
+
+func main() {
+
+	bands := GetArtist()
+	for _, band := range bands {
+		http.HandleFunc("/artist/"+band.Name, artistHandler(band.Id))
+	}
+
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	http.HandleFunc("/", firstPageHandler)
+	http.HandleFunc("/artist", mainPageHandler)
+
+	fmt.Println("starting server at port 8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func firstPageHandler(w http.ResponseWriter, r *http.Request) {
+	if !checkURL(r.URL.Path) {
+		fmt.Println("error 404, page not found")
+		fmt.Fprintf(w, "404 Not Found")
+		return
+	}
+
+	tmpl, err := template.ParseFiles("templates/first.html")
+	if err != nil {
+		http.Error(w, "Error parsing template", http.StatusInternalServerError)
+		return
+	}
+
+	err = tmpl.Execute(w, nil)
+	if err != nil {
+		http.Error(w, "Error executing template", http.StatusInternalServerError)
+		return
+	}
+}
+
+func mainPageHandler(w http.ResponseWriter, r *http.Request) {
+	if !checkURL(r.URL.Path) {
+		fmt.Println("error 404, page not found")
+		fmt.Fprintf(w, "404 Not FOund")
+		return
+	}
+
+	artists := GetArtist()
+	tmpl, err := template.ParseFiles("templates/artisthandler.html")
+	if err != nil {
+		http.Error(w, "Error parsing template", http.StatusInternalServerError)
+		return
+	}
+	fmt.Println("Templated is parsed lol")
+
+	err = tmpl.Execute(w, artists)
+	if err != nil {
+		http.Error(w, "Error executing template", http.StatusInternalServerError)
+		return
+	}
+	fmt.Println("checkcheck")
+}
+
+func artistHandler(id int) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Found band")
+		Band := Bandinfo{
+			Relations: GetRelation(id),
+			Artist:    GetArtist()[id-1],
+		}
+		tmpl, _ := template.ParseFiles("templates/artistinfo.html")
+		tmpl.Execute(w, Band)
+	}
+}
+
+var client *http.Client
+
+func GetJson(url string, target interface{}) error {
+	resp, err := client.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	return json.NewDecoder(resp.Body).Decode(target)
+}
+
+// struct for the bands
+type Artist struct {
+	Id           int      `json:"id"`
+	Image        string   `json:"image"`
+	Name         string   `json:"name"`
+	Members      []string `json:"members"`
+	CreationDate int      `json:"creationdate"`
+	FirstAlbum   string   `json:"firstalbum"`
+}
+
+type Relations struct {
+	DatesLocations interface{} `json:"datesLocations"`
+}
+
+type Bandinfo struct {
+	Relations Relations
+	Artist    Artist
+}
+
+func GetArtist() []Artist {
+	client = &http.Client{Timeout: 10 * time.Second}
+	url := "https://groupietrackers.herokuapp.com/api/artists"
+	var artists []Artist
+	err := GetJson(url, &artists)
+	if err != nil {
+		fmt.Printf("error getting Artist: %s\n", err.Error())
+	}
+	return artists
+}
+
+func GetRelation(id int) Relations {
+	str := strconv.Itoa(id)
+	url := "https://groupietrackers.herokuapp.com/api/relation/" + str
+	var relations Relations
+	err := GetJson(url, &relations)
+	if err != nil {
+		fmt.Printf("error getting Artist: %s\n", err.Error())
+	}
+	return relations
+}
+
+func checkURL(url string) bool {
+	artInfo := GetArtist()
+	for _, i := range artInfo {
+		if url == "/"+i.Name || url == "/" || url == "/artist" {
+			return true
+		}
+	}
+	return false
+}
